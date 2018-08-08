33b5ea72a40155b25faaa06f912cf3e199e06ab7
ben-dalton <benadalton@gmail.com>
2018-08-08 11:37:00 -0400

    updates layout for FAQ page

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import egg from '../assets/egg.png';
+
+const eggStyle = {
+  width: '50%',
+  position: 'relative',
+  padding: '0 20px',
+  left: '-20px',
+  height: 'auto',
+  minWidth: '150px',
+  maxWidth: '300px',
+  float: 'left',
+  marginRight: '2rem',
+};
 
 const FAQ = () => (
-  <div style={{ clear: 'both' }}>
+  <div style={{ marginTop: '30px', clear: 'both' }}>
     <div style={{ fontSize: '18px', padding: '20px 0' }}>
-      {"I've got more questions than answers..."}
+      <img className="faqCurve" alt="egg" src={egg} style={eggStyle} />
+      <h1>Frequently Asked Questions</h1>
+      <p style={{ padding: '20px' }}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum placeat
+        cupiditate rerum soluta, optio alias ad mollitia similique harum ipsam
+        labore odit, deserunt quod sequi maxime ab quas, obcaecati praesentium?
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum placeat
+        cupiditate rerum soluta, optio alias ad mollitia similique harum ipsam
+        labore odit, deserunt quod sequi maxime ab quas, obcaecati praesentium?
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum placeat
+        cupiditate rerum soluta, optio alias ad mollitia similique harum ipsam
+        labore odit, deserunt quod sequi maxime ab quas, obcaecati praesentium?
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum placeat
+        cupiditate rerum soluta, optio alias ad mollitia similique harum ipsam
+        labore odit, deserunt quod sequi maxime ab quas, obcaecati praesentium?
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum placeat
+        cupiditate rerum soluta, optio alias ad mollitia similique harum ipsam
+        labore odit, deserunt quod sequi maxime ab quas, obcaecati praesentium?
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum placeat
+        cupiditate rerum soluta, optio alias ad mollitia similique harum ipsam
+        labore odit, deserunt quod sequi maxime ab quas, obcaecati praesentium?
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum placeat
+        cupiditate rerum soluta, optio alias ad mollitia similique harum ipsam
+        labore odit, deserunt quod sequi maxime ab quas, obcaecati praesentium?
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum placeat
+        cupiditate rerum soluta, optio alias ad mollitia similique harum ipsam
+        labore odit, deserunt quod sequi maxime ab quas, obcaecati praesentium?
+      </p>
     </div>
   </div>
 );
